88fb3c10d9241bfc12200265e842f9e0a926b49b
Samvelizak <izaksam25@gmail.com>
2024-01-11 16:45:14 +0530

    Commit Login page Test Cases

--- src/test/java/LoginPageTestCase.java
+++ src/test/java/LoginPageTestCase.java
@@ -14,45 +14,58 @@ public class LoginPageTestCase {
     public void setUp(){
         driver.get("");
     }
-    @Test  //  Test case 1
-    @DisplayName("")
+    @Test  //  Test case 1 Vaild Username and Password
+    @DisplayName("LoginPage/TS_001/TC_001")
     public void TC_001(){
 
 
     }
 
-    @Test// Test case 2
-    @DisplayName("")
+    @Test// Test case 2 invalid username and valid password
+    @DisplayName("LoginPage/TS_001/TC_002")
     public void TC_002(){
 
     }
 
-    @Test// Test case 3
-    @DisplayName("")
+    @Test// Test case 3 valid password and invalid password
+    @DisplayName("LoginPage/TS_001/TC_003")
     public void TC_003(){
 
     }
 
-    @Test// Test case 4
-    @DisplayName("")
+    @Test// Test case 4 invalid username and password
+    @DisplayName("LoginPage/TS_001/TC_004")
     public void TC_004(){
 
     }
 
-    @Test// Test case 5
-    @DisplayName("")
+
+    @Test// Test case 6 To exceed the data length of username
+    @DisplayName("LoginPage/TS_001/TC_005")
     public void TC_005(){
 
     }
-
-    @Test// Test case 6
-    @DisplayName("")
+    @Test // Test case 7 To exceed the data length of password
+    @DisplayName("LoginPage/TS_001/TC_006")
     public void TC_006(){
 
     }
 
+    @Test//Test case 8 leave the username empty and click it
+    @DisplayName("LoginPage/TS_001/TC_007")
+    public void TC_007(){
 
-    @Test
+    }
+
+    @Test//Test case 9 leave the password empty and click it
+    @DisplayName("LoginPage/TS_001/TC_008")
+    public void TC_008(){
+
+    }
+
+
+
+
 
     @AfterEach
     public void tearDown() {

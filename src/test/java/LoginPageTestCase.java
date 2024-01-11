f191ea6c814c53c4d6219aa584adabd92adfd316
Samvelizak <izaksam25@gmail.com>
2024-01-11 20:27:41 +0530

    commit-- added test cases for login and selecting add or view page

--- src/test/java/LoginPageTestCase.java
+++ src/test/java/LoginPageTestCase.java
@@ -1,7 +1,4 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -17,6 +14,11 @@ public class LoginPageTestCase {
     @Test  //  Test case 1 Vaild Username and Password
     @DisplayName("LoginPage/TS_001/TC_001")
     public void TC_001(){
+        driver.findElement(By.id("")).sendKeys("");
+        driver.findElement(By.id("")).sendKeys("");
+        driver.findElement(By.id("")).click();
+        Assertions.assertEquals("","");
+
 
 
     }
@@ -24,18 +26,30 @@ public class LoginPageTestCase {
     @Test// Test case 2 invalid username and valid password
     @DisplayName("LoginPage/TS_001/TC_002")
     public void TC_002(){
+        driver.findElement(By.id("")).sendKeys("");
+        driver.findElement(By.id("")).sendKeys("");
+        driver.findElement(By.id("")).click();
+        Assertions.assertEquals("","");
 
     }
 
     @Test// Test case 3 valid password and invalid password
     @DisplayName("LoginPage/TS_001/TC_003")
     public void TC_003(){
+        driver.findElement(By.id("")).sendKeys("");
+        driver.findElement(By.id("")).sendKeys("");
+        driver.findElement(By.id("")).click();
+        Assertions.assertEquals("","");
 
     }
 
     @Test// Test case 4 invalid username and password
     @DisplayName("LoginPage/TS_001/TC_004")
     public void TC_004(){
+        driver.findElement(By.id("")).sendKeys("");
+        driver.findElement(By.id("")).sendKeys("");
+        driver.findElement(By.id("")).click();
+        Assertions.assertEquals("","");
 
     }
 
@@ -43,23 +57,39 @@ public class LoginPageTestCase {
     @Test// Test case 6 To exceed the data length of username
     @DisplayName("LoginPage/TS_001/TC_005")
     public void TC_005(){
+        driver.findElement(By.id("")).sendKeys("");
+        driver.findElement(By.id("")).sendKeys("");
+        driver.findElement(By.id("")).click();
+        Assertions.assertEquals("","");
 
     }
     @Test // Test case 7 To exceed the data length of password
     @DisplayName("LoginPage/TS_001/TC_006")
     public void TC_006(){
+        driver.findElement(By.id("")).sendKeys("");
+        driver.findElement(By.id("")).sendKeys("");
+        driver.findElement(By.id("")).click();
+        Assertions.assertEquals("","");
 
     }
 
     @Test//Test case 8 leave the username empty and click it
     @DisplayName("LoginPage/TS_001/TC_007")
     public void TC_007(){
+        driver.findElement(By.id("")).sendKeys("");
+        driver.findElement(By.id("")).sendKeys("");
+        driver.findElement(By.id("")).click();
+        Assertions.assertEquals("","");
 
     }
 
     @Test//Test case 9 leave the password empty and click it
     @DisplayName("LoginPage/TS_001/TC_008")
     public void TC_008(){
+        driver.findElement(By.id("")).sendKeys("");
+        driver.findElement(By.id("")).sendKeys("");
+        driver.findElement(By.id("")).click();
+        Assertions.assertEquals("","");
 
     }
 
--- src/test/java/SelectingAddOrViewTestCases.java
+++ src/test/java/SelectingAddOrViewTestCases.java
@@ -1,2 +1,46 @@
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SelectingAddOrViewTestCases {
+    public ChromeDriver driver = new ChromeDriver();
+    @BeforeEach
+    public void setUp(){
+        driver.get("");
+    }
+
+    @Test
+    @DisplayName("Select/TS_/TC_001")
+    public void STC_001(){
+        driver.findElement(By.id("")).click();
+        Assertions.assertEquals("","");
+
+    }
+    @Test
+    @DisplayName("Select/TS_/TC_002")
+    public void STC_002(){
+        driver.findElement(By.id("")).click();
+        Assertions.assertEquals("","");
+
+    }
+    @Test
+    @DisplayName("Select/TS_/TC_003")
+    public void STC_003(){
+        driver.findElement(By.id("")).click();
+        Assertions.assertEquals("","");
+
+    }
+    @AfterEach
+    public void tearDown() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
 }
